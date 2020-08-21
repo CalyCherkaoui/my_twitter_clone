4398ead3c1912b17a8939c0e35f9cbf93e0037c5
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-08-21 17:58:44 -0500

    Add markup for user registrations

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,5 +1,6 @@
 class GossipsController < ApplicationController
   before_action :set_gossip, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, expept: [:index, :show]
 
   # GET /gossips
   # GET /gossips.json
