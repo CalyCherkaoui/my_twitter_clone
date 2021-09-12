c8cea109e8be5682e0cff909e6ccc1767d6b9c6b
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-09-12 13:29:54 -0500

    Change to pg database

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem 'jbuilder', '~> 2.7'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3', '~> 1.4'
+gem 'pg'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_08_22_220545) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "gossips", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
